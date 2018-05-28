f2043611f559f6cd28060f1ef32ae74bcb425204
Michael Barrett <mb@abouthalf.com>
2018-05-28 07:46:05 -0700

    Re-tool using webpack
    * HTML generation
    * Less for CSS
    * tyle=module for scripts

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const {resolve, join} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebPackPlugin = require('script-ext-html-webpack-plugin');
+const LessPluginAutoPrefix = require('less-plugin-autoprefix');
+const CleanCSSPlugin = require('less-plugin-clean-css');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    context: resolve(__dirname, 'src'),
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        filename: '[name].bundle.[hash].js',
+        path: resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            noIeCompat: true,
+                            plugins: [
+                                new LessPluginAutoPrefix({browsers: ["last 2 versions"]}),
+                                new CleanCSSPlugin({advanced: true})
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+        contentBase: join(__dirname, 'dist'),
+        compress: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(
+            [resolve(__dirname, 'dist')],
+            {
+                root: __dirname,
+                exclude: ['.nojekyll', 'CNAME']
+            }
+        ),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, 'src/index.html')
+        }),
+        new ScriptExtHtmlWebPackPlugin({
+            defaultAttribute: 'defer',
+            module: 'app'
+        }) 
+    ]
+}
